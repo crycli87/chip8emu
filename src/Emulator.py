a515903fbe750d28dc785bf124e9afcb1c383f70
MOCHY <testttttttttttt@gmail.com>
2020-07-13 18:17:37 +0900

    レジスタ用意

--- src/Emulator.py
+++ src/Emulator.py
@@ -21,6 +21,14 @@ class Emulator :
 
     def __init__(self, rom):
         self._load_rom(rom)
+        self._memory = [0x0]*4096
+        self._registers = [0x0]*0xf # V1,V2 ~ V9,Va,Vb,~ Vf までの16個。原則16進数をindexとしてアクセスする
+        self._pc = 0x0
+        self._sp = 0x0 # スタックポインタ 8bit
+        self._dt = 0x0
+        self._st = 0x0
+        self._i = 0x0
+        self._stacks = [0x0]*16
         self._font_address = 0x0
         self._load_font(self._font_address)
         hz = threading.Thread(target=self._pulse_60hz)
@@ -29,6 +37,9 @@ class Emulator :
     def _pulse_60hz(self):
         t = threading.Timer(1/60, self._pulse_60hz)
         t.start()
+        if self._dt<0 : self._dt-=1
+        if self._st<0 : self._st-=1
+
     def _load_font(self, font_address):
         for i in range(len(Emulator._fonts)):
             self._memory[font_address+i] = Emulator._fonts[i]
@@ -48,6 +59,6 @@ class Emulator :
         screen.refresh()
     
     def tick(self):
-        None
+        
 
 
